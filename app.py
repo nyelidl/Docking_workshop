31d26f9110dc51a8792da39919354472513ed526
nyelidl <120070650+nyelidl@users.noreply.github.com>
2026-02-28 10:15:22 +0900

    Update app.py

--- app.py
+++ app.py
@@ -489,7 +489,7 @@ def _receptor_section(pfx: str, wdir: Path, step_label: str):
                 (st.session_state.get(pfx+"receptor_fh"),
                  {"cartoon": {"color": "spectrum", "opacity": 0.65}}),
                 (st.session_state.get(pfx+"box_pdb"),
-                 {"line": {"color": "gray", "linewidth": 2.2}}),
+                 {"stick": {"radius":0.2, "color": "gray"}}),
             ]:
                 if path and os.path.exists(path):
                     v3.addModel(open(path).read(), "pdb")
